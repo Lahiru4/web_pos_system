2e1a58d5c80e45ef12d73ec6663f73b8d70f7609
Lahiru4 <lahiruindusara04@gmail.com>
2023-11-12 13:44:26 +0530

    edit

--- controller/indexController..js
+++ controller/indexController..js
@@ -4,25 +4,25 @@ $('#Order').css('display','none');
 $('#orderDetails').css('display','none');
 
 
-$('.side-menu>li').eq(1).on('click', () => {
+$('.side-menu>li').eq(0).on('click', () => {
     $('#Customer').css('display','none');
     $('#Item').css('display','none');
     $('#Order').css('display','block');
     $('#orderDetails').css('display','none');
 })
-$('.side-menu>li').eq(2).on('click', () => {
+$('.side-menu>li').eq(1).on('click', () => {
     $('#Customer').css('display','block');
     $('#Item').css('display','none');
     $('#Order').css('display','none');
     $('#orderDetails').css('display','none');
 })
-$('.side-menu>li').eq(3).on('click', () => {
+$('.side-menu>li').eq(2).on('click', () => {
     $('#Item').css('display','block');
     $('#Customer').css('display','none');
     $('#Order').css('display','none');
     $('#orderDetails').css('display','none');
 })
-$('.side-menu>li').eq(4).on('click', () => {
+$('.side-menu>li').eq(3).on('click', () => {
     $('#Item').css('display','none');
     $('#Customer').css('display','none');
     $('#Order').css('display','none');
