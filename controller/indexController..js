8bbf859ff54e8940c326d1f108595f0c0e0d16c3
Lahiru4 <lahiruindusara04@gmail.com>
2023-11-12 13:28:50 +0530

    edit

--- controller/indexController..js
+++ controller/indexController..js
@@ -8,7 +8,7 @@ $('.side-menu>li').eq(1).on('click', () => {
     $('#Customer').css('display','none');
     $('#Item').css('display','none');
     $('#Order').css('display','block');
-    $('#orderDetails').css('display','block');
+    $('#orderDetails').css('display','none');
 })
 $('.side-menu>li').eq(2).on('click', () => {
     $('#Customer').css('display','block');
@@ -21,4 +21,10 @@ $('.side-menu>li').eq(3).on('click', () => {
     $('#Customer').css('display','none');
     $('#Order').css('display','none');
     $('#orderDetails').css('display','none');
+})
+$('.side-menu>li').eq(4).on('click', () => {
+    $('#Item').css('display','none');
+    $('#Customer').css('display','none');
+    $('#Order').css('display','none');
+    $('#orderDetails').css('display','block');
 })
